8439a3eeab87a023716d8d1dfe46f8688704d5af
steffen3112 <schwarzs395@gmail.com>
2018-09-13 20:38:12 +0200

    Updated server.js and pckg.json

--- client/server.js
+++ client/server.js
@@ -6,6 +6,10 @@ const app = express();
 // in the dist directory
 app.use(express.static(__dirname + '/www'));
 
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/www/index.html'));   
+});  
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
