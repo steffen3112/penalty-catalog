12bfa858803d4815b911380b777c13e28f417d77
steffen3112 <schwarzs395@gmail.com>
2018-09-30 14:49:15 +0200

    sample picture in dialog

--- client/src/app/pages/usercontent/usercontent.page.ts
+++ client/src/app/pages/usercontent/usercontent.page.ts
@@ -9,6 +9,8 @@ import { NavController,ModalController, NavParams} from '@ionic/angular';
 })
 export class UsercontentPage implements OnInit {
 
+  public userPicture = `https://randomuser.me/api/portraits/thumb/men/${Math.round((Math.random()*100))}.jpg`;
+
   @Input() user: User;
 
   constructor(private nav: NavController, public modalCtrl: ModalController, private params: NavParams) { 
